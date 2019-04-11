3621b8ff5f3dffc3e83e20cbdc6255447fbb5e4b
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-04-11 12:02:26 -0400

    CA5368: Set the ViewStateUserKey property in Page OnInit
    GECO-480

--- GECO/GECO.Page.vb
+++ GECO/GECO.Page.vb
@@ -1,4 +1,9 @@
 ﻿Public Class Page
     Inherits UI.Page
 
+    Protected Overrides Sub OnInit(e As EventArgs)
+        ViewStateUserKey = Session.SessionID
+        MyBase.OnInit(e)
+    End Sub
+
 End Class
