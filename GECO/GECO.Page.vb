94ae305d0deb1a611748393159db45c4234e79c4
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-04-11 12:02:26 -0400

    Revert "CA5368: Set the ViewStateUserKey property in Page OnInit"
    This reverts commit 3621b8ff5f3dffc3e83e20cbdc6255447fbb5e4b.

--- GECO/GECO.Page.vb
+++ GECO/GECO.Page.vb
@@ -1,9 +1,4 @@
 ﻿Public Class Page
     Inherits UI.Page
 
-    Protected Overrides Sub OnInit(e As EventArgs)
-        ViewStateUserKey = Session.SessionID
-        MyBase.OnInit(e)
-    End Sub
-
 End Class
