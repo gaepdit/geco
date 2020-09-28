af01ac55c172f9a77206d6a5281764690135c522
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-09-28 15:41:55 -0400

    Bump version: 2020.9.28

--- GECO/My Project/AssemblyInfo.vb
+++ GECO/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("2020.9.28.*")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
