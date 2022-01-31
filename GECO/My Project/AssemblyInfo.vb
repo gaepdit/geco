0537aeb8863350a763911256931ed0312b2e338f
Doug Waldron <douglas.waldron@dnr.ga.gov>
2022-01-31 08:45:52 -0500

    Bump version: 2022.1.31

--- GECO/My Project/AssemblyInfo.vb
+++ GECO/My Project/AssemblyInfo.vb
@@ -6,7 +6,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCompany("State of Georgia")>
 <Assembly: AssemblyProduct("GECO")>
 <Assembly: AssemblyCopyright("Copyright © 2021")>
-<Assembly: AssemblyVersion("2021.10.15")>
+<Assembly: AssemblyVersion("2022.1.31")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: ComVisible(False)>
 <Assembly: NeutralResourcesLanguage("en-US")>
