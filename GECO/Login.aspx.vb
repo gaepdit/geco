d43438898dabc177f4dfd49dac62142303e258c9
Pat <pat.tran@dnr.ga.gov>
2022-10-31 23:39:57 -0400

    got the timer working, but the label is not updating

--- GECO/Login.aspx.vb
+++ GECO/Login.aspx.vb
@@ -1,8 +1,17 @@
-﻿Imports GECO.EmailTemplates
+﻿Imports System.ComponentModel
+Imports System.Threading
+Imports System.Threading.Tasks
+Imports System
+Imports GECO.EmailTemplates
 Imports GECO.GecoModels
 
 Partial Class Login
     Inherits Page
+    ' Apparently this is a static variable in VB.NET
+    ' (need 'shared' procedure to use static in that procedure)
+    Shared failedSignInAttempts As Integer
+    Shared isThrottled As Boolean
+    Shared timeoutTimer As New CountDownTimer(2, 0)
 
 #Region " Page load "
 
@@ -32,52 +41,100 @@ Partial Class Login
 
     Protected Sub btnSignIn_Click(sender As Object, e As EventArgs) Handles btnSignIn.Click
         SignIn()
+
+    End Sub
+
+    Protected Sub Delay_SignIn()
+        ' btnSignIn.Enabled = False
+        isThrottled = True
+        Debug.Print("Delay " & failedSignInAttempts)
+        Thread.Sleep(20000)
+        isThrottled = False
+        ' btnSignIn.Enabled = True
     End Sub
 
     Protected Sub SignIn()
         lblMessage.Visible = False
         lblUnconfirmed.Visible = False
 
-        Dim gecoUser As New GecoUser
-        Dim userSession As New UserSession
+        'Static timeoutTimer As New CountDownTimer
+        ' set timer for 1 minutes
+        'timeoutTimer.SetTime(1, 0)
 
-        Dim loginResult As LoginResult = LogInUser(txtUserId.Text, txtPassword.Text, chkRememberMe.Checked, gecoUser, userSession)
+        ' Set the action each time the timer ticks down
+        timeoutTimer.TimeChanged = New Action(Sub()
+                                                  lblMessage.Text = timeoutTimer.TimeLeft.ToString("mm\:ss")
+                                                  LoginUpdatePanel.Update()
+                                                  Debug.Print("Remaining time " & timeoutTimer.TimeLeft.ToString("mm\:ss"))
+                                                  lblMessage.Visible = True
 
-        Select Case loginResult
-            Case LoginResult.Invalid
-                lblMessage.Visible = True
 
-            Case LoginResult.AccountUnconfirmed
-                lblUnconfirmed.Visible = True
+                                              End Sub)
 
-            Case LoginResult.Success
-                If gecoUser.UserId = 0 Then
+        timeoutTimer.CountDownFinished = New Action(Sub()
+                                                        lblMessage.Visible = False
+                                                        timeoutTimer.Stop()
+                                                    End Sub)
+
+        Debug.Print("failedSignInAttempts: " & failedSignInAttempts)
+        Debug.Print("timeoutTimer.IsRunning: " & timeoutTimer.IsRunning)
+
+        If timeoutTimer.IsRunning Then
+            ' display the error message
+            Debug.Print("Throttled: " & isThrottled)
+        Else
+            ' if the login screen is currently not throttled
+            Dim gecoUser As New GecoUser
+            Dim userSession As New UserSession
+
+            Dim loginResult As LoginResult = LogInUser(txtUserId.Text, txtPassword.Text, chkRememberMe.Checked, gecoUser, userSession)
+
+            Select Case loginResult
+                Case LoginResult.Invalid
+                    failedSignInAttempts += 1
+                    lblMessage.Text += failedSignInAttempts.ToString
+                    lblMessage.Visible = True
+
+                    If failedSignInAttempts > 2 Then
+                        'Dim thread As New Thread(AddressOf Delay_SignIn)
+                        'hread.Start()
+                        timeoutTimer.Start()
+                    End If
+
+                Case LoginResult.AccountUnconfirmed
+                    lblUnconfirmed.Visible = True
+
+                Case LoginResult.Success
+                    If gecoUser.UserId = 0 Then
+                        Response.Redirect("~/ErrorPage.aspx", False)
+                    End If
+
+                    failedSignInAttempts = 0
+
+                    SessionAdd(GecoSession.CurrentUser, gecoUser)
+
+                    If chkRememberMe.Checked Then
+                        CreateSessionCookie(userSession)
+                    Else
+                        ClearCookie(SessionCookie)
+                    End If
+
+                    If gecoUser.ProfileUpdateRequired Then
+                        Response.Redirect("~/Account/?action=updateprofile")
+                    End If
+
+                    Dim strRedirect As String = Request.QueryString("ReturnUrl")
+
+                    If String.IsNullOrEmpty(strRedirect) Then
+                        Response.Redirect("~/Home/")
+                    Else
+                        Response.Redirect(strRedirect)
+                    End If
+
+                Case Else 'Some Error
                     Response.Redirect("~/ErrorPage.aspx", False)
-                End If
-
-                SessionAdd(GecoSession.CurrentUser, gecoUser)
-
-                If chkRememberMe.Checked Then
-                    CreateSessionCookie(userSession)
-                Else
-                    ClearCookie(SessionCookie)
-                End If
-
-                If gecoUser.ProfileUpdateRequired Then
-                    Response.Redirect("~/Account/?action=updateprofile")
-                End If
-
-                Dim strRedirect As String = Request.QueryString("ReturnUrl")
-
-                If String.IsNullOrEmpty(strRedirect) Then
-                    Response.Redirect("~/Home/")
-                Else
-                    Response.Redirect(strRedirect)
-                End If
-
-            Case Else 'Some Error
-                Response.Redirect("~/ErrorPage.aspx", False)
-        End Select
+            End Select
+        End If
 
     End Sub
 
