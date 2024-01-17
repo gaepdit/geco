4150e9442d9275cf0ed836ddd519fa9871d6a193
Doug Waldron <douglas.waldron@dnr.ga.gov>
2024-01-17 10:43:39 -0500

    Update copyright year

--- GECO/My Project/AssemblyInfo.vb
+++ GECO/My Project/AssemblyInfo.vb
@@ -5,7 +5,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("GECO")>
 <Assembly: AssemblyCompany("State of Georgia")>
 <Assembly: AssemblyProduct("GECO")>
-<Assembly: AssemblyCopyright("Copyright © 2023")>
+<Assembly: AssemblyCopyright("Copyright © 2024")>
 <Assembly: AssemblyVersion("2023.12.4")>
 <Assembly: AssemblyFileVersion("1.0.0.0")>
 <Assembly: ComVisible(False)>
