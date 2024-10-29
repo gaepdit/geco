e67fe5996853c82ee3f9145f6e3b6c472c7b314b
Doug Waldron <737326+dougwaldron@users.noreply.github.com>
2024-10-29 15:25:16 -0400

    Improve permit application table (#613)
    * Remove unused file
    
    * Display permit number in permit application table

---
+++ GECO/_DAL/NullableDates.vb
@@ -0,0 +1,12 @@
+﻿Imports GaEpd
+
+Module NullableDates
+    Public Function GetNullableDateTime(obj As Object) As DateTime?
+        Dim newDate As Date? = DBUtilities.GetNullableDateTime(obj)
+        If newDate IsNot Nothing AndAlso newDate = New Date(1776, 7, 4) Then
+            Return Nothing
+        End If
+        Return newDate
+    End Function
+
+End Module
