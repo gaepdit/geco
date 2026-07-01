945d8388deddb0f6ef9b4fee5fe8313a60d80b4e
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-07-01 09:11:18 -0400

    Redirect the files folder to the home page

---
+++ GECO/files/Default.aspx.vb
@@ -0,0 +1,8 @@
+﻿Partial Class FilesRedirect
+    Inherits Page
+
+    Private Sub FilesRedirect_Load(sender As Object, e As EventArgs) Handles Me.Load
+        CompleteRedirect("~/")
+    End Sub
+
+End Class
