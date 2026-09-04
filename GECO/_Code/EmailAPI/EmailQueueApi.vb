399b5a92577d2116e03c3269bec5f9d0fc5afe2d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-09-04 15:09:38 -0400

    Add email queue functions

---
+++ GECO/_Code/EmailAPI/EmailQueueApi.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Net.Http
+Imports System.Text.Json
+Imports System.Threading.Tasks
+
+Namespace EmailAPI
+    Public Module EmailQueueApi
+
+        Private Const SendEndpoint As String = "add"
+        Private Const SendForBatchEndpoint As String = "add-to-batch"
+        Private Const BatchDetailsEndpoint As String = "batch-details"
+        'Private Const BatchStatusEndpoint As String = "batch-status"
+
+        Private Async Function CallEmailApiAsync(Of T)(payload As Object, endpoint As String) As Task(Of T)
+            Dim _baseUri As String = ConfigurationManager.AppSettings("EmailQueueApiUrl")
+            Dim _clientID As String = ConfigurationManager.AppSettings("EmailQueueClientId")
+            Dim _apiKey As String = ConfigurationManager.AppSettings("EmailQueueApiKey")
+
+            Dim requestContent As New StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json")
+            Dim responseBody As String
+
+            Using client As New HttpClient()
+                client.DefaultRequestHeaders.Add("X-API-Key", _apiKey)
+                client.DefaultRequestHeaders.Add("X-Client-ID", _clientID)
+
+                Dim response As HttpResponseMessage = Await client.PostAsync(New Uri(_baseUri & endpoint), requestContent)
+                response.EnsureSuccessStatusCode()
+                responseBody = Await response.Content.ReadAsStringAsync()
+
+                Return JsonSerializer.Deserialize(Of T)(responseBody)
+            End Using
+        End Function
+
+        Public Function QueueEmail(email As Email) As Task(Of EmailQueueApiResponse)
+            Return QueueEmail(New List(Of Email) From {email})
+        End Function
+
+        Public Async Function QueueEmail(emails As List(Of Email)) As Task(Of EmailQueueApiResponse)
+            Try
+                Dim response As EmailQueueResponseBody = Await CallEmailApiAsync(Of EmailQueueResponseBody)(emails, SendEndpoint)
+                Return EmailQueueApiResponse.Ok(response)
+            Catch e As Exception
+                Return EmailQueueApiResponse.Failed
+            End Try
+        End Function
+
+        Public Async Function QueueEmailWithBatchID(emails As List(Of Email), batchId As Guid) As Task(Of EmailQueueApiResponse)
+            Dim emailForBatchRequest As New EmailForBatchRequest() With {
+                .Emails = emails,
+                .BatchId = batchId
+            }
+
+            Try
+                Dim response As EmailQueueResponseBody = Await CallEmailApiAsync(Of EmailQueueResponseBody)(emailForBatchRequest, SendForBatchEndpoint)
+                Return EmailQueueApiResponse.Ok(response)
+            Catch e As Exception
+                Return EmailQueueApiResponse.Failed
+            End Try
+        End Function
+
+        Public Async Function GetBatchDetails(batchId As Guid) As Task(Of EmailBatchDetails)
+            Dim batchRequest As New BatchRequest() With {.BatchId = batchId}
+
+            Try
+                Dim response As List(Of EmailTask) = Await CallEmailApiAsync(Of List(Of EmailTask))(batchRequest, BatchDetailsEndpoint)
+                Return EmailBatchDetails.Ok(response)
+            Catch e As Exception
+                Return EmailBatchDetails.Failed
+            End Try
+        End Function
+
+    End Module
+End Namespace
---
+++ GECO/_Code/EmailAPI/EmailQueue_Response.vb
@@ -0,0 +1,78 @@
+﻿Imports System.ComponentModel
+
+Namespace EmailAPI
+    Public Class EmailQueueResponseBody
+        Public Property Status As String
+        Public Property Count As Integer
+        Public Property BatchId As String
+    End Class
+
+    Public Class EmailQueueApiResponse
+        Public Property Status As String
+        Public Property Body As EmailQueueResponseBody
+
+        Public Shared ReadOnly Property Failed As EmailQueueApiResponse = New EmailQueueApiResponse With {
+            .Status = "Failed",
+            .Body = Nothing
+        }
+
+        Public Shared Function Ok(body As EmailQueueResponseBody) As EmailQueueApiResponse
+            Return New EmailQueueApiResponse With {
+                .Status = body.Status,
+                .Body = body
+            }
+        End Function
+    End Class
+
+    Public Class EmailTask
+        Public Property Counter As Integer
+        Public Property Status As String
+        Public Property AttemptedAt As Date
+        Public Property From As String
+        Public Property Subject As String
+        Public Property Recipients As List(Of String)
+        Public Property FailureReason As String
+    End Class
+
+    Public Class EmailBatchDetails
+        Public Property Status As String
+        Public Property Emails As List(Of EmailTask)
+
+        Public Shared ReadOnly Property Failed As EmailBatchDetails = New EmailBatchDetails With {
+            .Status = "Failed",
+            .Emails = Nothing
+        }
+
+        Public Shared Function Ok(emails As List(Of EmailTask)) As EmailBatchDetails
+            Return New EmailBatchDetails With {
+                .Status = "Success",
+                .Emails = emails
+            }
+        End Function
+    End Class
+
+    <Serializable>
+    Public Class EmailTaskViewModel
+        Public Sub New(email As EmailTask)
+            Id = email.Counter
+            Status = email.Status
+            Sent = If(email.AttemptedAt = Nothing, email.AttemptedAt.ToLocalTime(), CType(Nothing, Date?))
+            Subject = email.Subject
+            Recipients = String.Join(", ", email.Recipients)
+            FailureReason = IfEmpty(email.FailureReason, "N/A")
+        End Sub
+
+        Public Property Id As Integer
+        Public Property Status As String
+        Public Property Sent As Date?
+        Public Property Subject As String
+        Public Property Recipients As String
+
+        <DisplayName("Failure Reason")>
+        Public Property FailureReason As String
+
+        Private Shared Function IfEmpty(value As String, defaultValue As String) As String
+            Return If(String.IsNullOrEmpty(value), defaultValue, value)
+        End Function
+    End Class
+End Namespace
